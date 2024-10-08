e34465664386d5ecb7a2d4629372b2f88ca14294
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-08 20:59:17 +0200

    Setting up the Developing Environment

--- github.api/src/main.ts
+++ github.api/src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
+import typescriptLogo from '..public/typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
