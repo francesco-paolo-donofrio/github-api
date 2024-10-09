a0f937362b7eb86b9340dc0fd9bd20fe25a51908
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-09 08:25:39 +0200

    Re-Setting up the Developing Environment

---
+++ index.js
@@ -0,0 +1,11 @@
+function isEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
+}
+;
+console.log(isEvenOrOdd(2));
+console.log(isEvenOrOdd(3));
