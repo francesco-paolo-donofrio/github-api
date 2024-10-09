0f0e0354f3327c5259065e15bbfccfbfd3180926
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-09 08:43:24 +0200

    Re-Setting up the Developing Environment

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+function isEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
+}
+;
+console.log(isEvenOrOdd(2));
+console.log(isEvenOrOdd(3));
+//# sourceMappingURL=index.js.map
